8575dbe3212eb4ed4b886dd700339859ad03f4ed
Neha Behera <125554694+neha0067@users.noreply.github.com>
2023-08-04 14:33:04 +0530

    Adds css for an appp (#4)

--- cart/src/Components/CartContainer.js
+++ cart/src/Components/CartContainer.js
@@ -6,15 +6,17 @@ import CartItem from "./CartItem";
 export default function CartContainer(){
     const { cart, total } = useContext(AppContext);
     return (
-        <main>
+        <main className="cart">
             <h2>YOUR BAG</h2>
-            <div>
+            <div className="mapped-cart">
                 {cart.map((item) => <CartItem key={item.id} cart={item}/>)}
             </div>
             <footer>
-                <hr />
-                <h2>Total</h2>
-                <span>{`$${total}`}</span>
+                <hr className="divider-line"/>
+                <div className="cart-total">
+                    <h2>Total</h2>
+                    <span>{`$${total}`}</span>
+                </div>
             </footer>
         </main>
     )
