2e7f30c08912ff9318b077576fc7fabbd2ad2588
neha0067 <beheraneha67@gmail.com>
2023-09-06 19:45:34 +0530

    Adds test cases

--- birthday-reminder/src/App.test.js
+++ birthday-reminder/src/App.test.js
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Birthdays', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Birthdays Today/i);
-  console.log(linkElement)
+  const linkElement = screen.getByText("5 Birthdays Today");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Birthdays', () => {
+  render(<App />);
+  const linkElement = screen.getByText("Bertie Yates");
   expect(linkElement).toBeInTheDocument();
 });
