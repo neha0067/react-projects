47ddf937523f8d440ede98e1b34692a1c6836f88
neha0067 <beheraneha67@gmail.com>
2024-05-11 14:47:14 +0530

    updates submit button

--- comfy-store/src/components/Header.tsx
+++ comfy-store/src/components/Header.tsx
@@ -15,7 +15,7 @@ function Header() {
   }
 
   return (
-    <header className=' bg-neutral py-2 text-neutral-content '>
+    <header className='bg-neutral py-2 text-neutral-content '>
       <div className='align-element flex justify-center sm:justify-end '>
         {user ? (
           <div className='flex gap-x-2 sm:gap-x-8 items-center'>
--- comfy-store/src/components/SubmitButton.tsx
+++ comfy-store/src/components/SubmitButton.tsx
@@ -1,17 +1,25 @@
-import { useNavigation } from "react-router-dom"
+import { useNavigation } from "react-router-dom";
 
-function SubmitButton({ text } : any) {
-    const navigation = useNavigation()
-    const isSubmitting = navigation.state === "submitting"
+function SubmitButton({ text }: any) {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
 
   return (
-    <button className="btn btn-primary btn-block" type="submit" disabled={isSubmitting}>{
-        isSubmitting ? <>
-            <span className="loading loading-spinner"></span>
-            sending...
-        </> : text || "submit"
-    }</button>
-  )
+    <button
+      className="btn btn-primary btn-block"
+      type="submit"
+      disabled={isSubmitting}
+    >
+      {isSubmitting ? (
+        <>
+          <span className="loading loading-spinner"></span>
+          sending...
+        </>
+      ) : (
+        text || "submit"
+      )}
+    </button>
+  );
 }
 
-export default SubmitButton
+export default SubmitButton;
